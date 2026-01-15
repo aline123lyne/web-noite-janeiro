d7bc3704ce91c58ccbd6bd2bdc77e9466e1b8776
aline123lyne <aline.lyne123@gmail.com>
2026-01-15 20:07:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function calcular(){
+    var n1 = parseFloat(document.getElementById('n1').value)
+    var n2 = parseFloat(document.getElementById('n2').value)
+    var operador = document.getElementById('operador').value
+    var resultado
+
+    switch (operador){
+
+        case '+':
+            resultado = n1 + n2
+            break;
+        case '-':
+            resultado = n1 -n2
+            break;
+        case '*':
+            resultado = n1 * n2
+            break;
+        case '/':
+            if (n2 !== 0){
+                resul
+                
+                tado = n1 / n2
+            }else{
+                resultado = 'Não se divide por 0'
+            }
+        default: "Operador Inválido"
+            break;
+
+    }
+    document.getElementById('resultado').textContent = resultado       
+    
+}
